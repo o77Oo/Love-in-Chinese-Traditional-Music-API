1267952dfaca6e5c9989915d86742feeb071aced
o77Oo <kaishibenben@gmail.com>
2022-04-02 21:43:44 -0600

    instrumentRoutes

--- routes/instrumentRoutes.js
+++ routes/instrumentRoutes.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const fs = require('fs');
 
 
-/*
- * Get collection of videos as array of objects
- */
+
 router.get('/', (req, res) => {
   fs.readFile('./data/instruments.json',  (err, data) => {
     const instrumentsData = JSON.parse(data);
@@ -13,20 +11,5 @@ router.get('/', (req, res) => {
   });
 });
 
-/*
- * Get single video by id
- */
-// router.get('/:id', (req, res) => {
-//   fs.readFile('./data/videos.json', 'utf8', (err, data) => {
-//     const videosData = JSON.parse(data);
-//     const foundVideo = videosData.find(
-//       (video) => video.id === req.params.id
-//     );
-//     if (foundVideo) {
-//       res.json(foundVideo);
-//     } else {
-//       res.send('no video found with that id');
-//     }
-//   });
-// });
+
 module.exports = router;
