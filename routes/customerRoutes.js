1e7bd9cf61e4592c8f72f33590576539950a65bd
o77Oo <kaishibenben@gmail.com>
2022-04-06 02:13:47 -0600

    detele test data

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,39 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
-
-
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 router.post("/", (req, res) => {
-    // read JSON file
-    fs.readFile("./data/customer.json", 'utf8', (err, data) => {
-console.log(err)
-console.log(data)
-const customerData = JSON.parse(data);
-      const newComment = {
-        id: uuidv4(), // creating unique id
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        emailaddress: req.body.emailaddress,
-        address: req.body.address,
-        phonenumber:req.body.phonenumber,
-        postalcode:req.body.postalcode,
-        province:req.body.province,
-        comments: req.body.comments,
+  fs.readFile("./data/customer.json", "utf8", (err, data) => {
+    const customerData = JSON.parse(data);
+    const newComment = {
+      id: uuidv4(),
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      emailaddress: req.body.emailaddress,
+      address: req.body.address,
+      phonenumber: req.body.phonenumber,
+      postalcode: req.body.postalcode,
+      province: req.body.province,
+      comments: req.body.comments,
+    };
 
-        
-      };
-      // push new object to local array
-      customerData.push(newComment);
-     
-      // write data back to JSON file
-      fs.writeFile("./data/customer.json", JSON.stringify(customerData), () => {
-        res.json({ message: 'data written to file', data: newComment });
-      });
+    customerData.push(newComment);
+
+    fs.writeFile("./data/customer.json", JSON.stringify(customerData), () => {
+      res.json({ message: "data written to file", data: newComment });
     });
   });
-  
-  
-  
-  module.exports = router;
+});
+
+module.exports = router;
--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,62 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
-/*
- * Get collection of videos as array of objects
- */
-router.get('/', (req, res) => {
-  fs.readFile('./data/videos.json', 'utf8', (err, data) => {
+router.get("/", (req, res) => {
+  fs.readFile("./data/videos.json", "utf8", (err, data) => {
     const videosData = JSON.parse(data);
     res.json(videosData);
   });
 });
 
-/*
- * Get single video by id
- */
-router.get('/:id', (req, res) => {
-  fs.readFile('./data/videos.json', 'utf8', (err, data) => {
+router.get("/:id", (req, res) => {
+  fs.readFile("./data/videos.json", "utf8", (err, data) => {
     const videosData = JSON.parse(data);
-    const foundVideo = videosData.find(
-      (video) => video.id === req.params.id
-    );
+    const foundVideo = videosData.find((video) => video.id === req.params.id);
     if (foundVideo) {
       res.json(foundVideo);
     } else {
-      res.send('no video found with that id');
+      res.send("no video found with that id");
     }
   });
 });
 
-/*
- * Create a new video
- */
-router.post('/:id', (req, res) => {
-  // read JSON file
-  console.log(req.body)
-  fs.readFile('./data/videos.json', 'utf8', (err, data) => {
-    const videosData = JSON.parse(data); console.log(videosData)
-    const findVideo = videosData.find((video) => {return video.id === req.params.id})
-    // create new object to push to local array before saving to videos.json
+router.post("/:id", (req, res) => {
+  fs.readFile("./data/videos.json", "utf8", (err, data) => {
+    const videosData = JSON.parse(data);
+    console.log(videosData);
+    const findVideo = videosData.find((video) => {
+      return video.id === req.params.id;
+    });
+
     const newComment = {
-      id: uuidv4(), // creating unique id
+      id: uuidv4(),
       name: req.body.name,
-      comment: req.body.comment, // incoming req.body
+      comment: req.body.comment,
       timestamp: Date.now(),
-      
     };
-    // push new object to local array
+
     findVideo.comments.push(newComment);
-    findVideo.comments.sort((comment1,comment2)=>{return comment2.timestamp - comment1.timestamp});
-    // write data back to JSON file
-    fs.writeFile('./data/videos.json', JSON.stringify(videosData), () => {
-      res.json({ message: 'data written to file', data: findVideo });
+    findVideo.comments.sort((comment1, comment2) => {
+      return comment2.timestamp - comment1.timestamp;
+    });
+
+    fs.writeFile("./data/videos.json", JSON.stringify(videosData), () => {
+      res.json({ message: "data written to file", data: findVideo });
     });
   });
 });
-
-
 
 module.exports = router;
